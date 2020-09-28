75eeced0e3487d9c08b695af9f478b66219a6822
Aigars Peda <aigarspeda@gmail.com>
2020-09-28 11:04:39 +0300

    Update types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,4 +5,10 @@ export type ScreamType = {
   likeCount: number;
   screamId: string;
   userHandle: string;
+  userImage: string;
+};
+
+export type ErrorsType = {
+  general?: null;
+  error?: null;
 };
