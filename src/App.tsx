91041f066143a54c2fb0fff152b35cf91fe2a6bf
Aigars Peda <aigarspeda@gmail.com>
2020-09-29 16:14:41 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./global.styles/App.scss";
-
 import { Switch, Route, BrowserRouter } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+
+// types
+import { TokenType } from "./types/types";
+
+// utils
+import AuthRoute from "./utils/authRoute/AuthRoute";
 
 // components
 import Home from "./pages/home/Home";
@@ -10,12 +16,32 @@ import SignUp from "./pages/signup/SignUp";
 import Navbar from "./components/navbar/Navbar";
 
 const App: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("FBIdToken");
+    if (token) {
+      const decodedToken: TokenType = jwtDecode(token);
+      if (decodedToken.exp * 1000 < Date.now()) {
+        window.location.href = "/login";
+        setIsAuthenticated(false);
+      } else {
+        setIsAuthenticated(true);
+      }
+    }
+  }, [isAuthenticated]);
+
   return (
     <BrowserRouter>
       <Navbar />
       <div className="App">
         <Switch>
-          <Route exact path="/" component={Home} />
+          <AuthRoute
+            exact
+            path="/"
+            component={Home}
+            isAuthenticated={isAuthenticated}
+          />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
         </Switch>
