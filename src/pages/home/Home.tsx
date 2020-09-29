421f355f03744e96e0ddb503a89463274aeea085
Aigars Peda <aigarspeda@gmail.com>
2020-09-29 08:58:36 +0300

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+// constants
+import { BASE_URL } from "../../constant";
+
+// types
+import { ScreamType } from "../../types/types";
+
+// components
+import Scream from "../../components/scream/Scream";
+import Spinner from "../../components/spinner/Spinner";
 
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  const [screams, setScreams] = useState<ScreamType[]>([]);
+
+  useEffect(() => {
+    fetch(BASE_URL + "/screams")
+      .then((response) => response.json())
+      .then((data) => setScreams(data))
+      .catch((err) => console.log("Error fetching screams: ", err));
+  }, []);
+
+  const recentScreamMarkup = screams.length ? (
+    screams.map((scream) => {
+      return <Scream key={scream.screamId} scream={scream} />;
+    })
+  ) : (
+    <div className="spinner">
+      <Spinner />
+    </div>
+  );
+
+  return (
+    <div className="home">
+      <div>{recentScreamMarkup}</div>
+      <div>Profile</div>
+    </div>
+  );
 };
 
 export default Home;
