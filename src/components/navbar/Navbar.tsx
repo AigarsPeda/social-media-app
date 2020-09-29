1a62a0b5819bfa00f890d24462c028a6e951162f
Aigars Peda <aigarspeda@gmail.com>
2020-09-29 07:39:23 +0300

    created Navbar components and styles for it

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../images/Logo";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="nav-bar">
-      <h3>Navbar</h3>
+    <nav className="navbar">
+      <div className="container">
+        <Link to="/" className="logo">
+          <Logo />
+        </Link>
+        <ul className="links">
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
