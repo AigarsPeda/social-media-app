3ffa7876f2c8e34151170fd7ee63111456c52b5c
Aigars Peda <aigarspeda@gmail.com>
2020-10-01 07:26:18 +0300

    if token adding it to axios header

---
+++ src/helpers/authToken.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const authToken = (token: string) => {
+  if (token) {
+    axios.defaults.headers.common["authorization"] = `Bearer ${token}`;
+  } else {
+    delete axios.defaults.headers.common["authorization"];
+  }
+};
+
+export default authToken;
