241441a9bd4ed9e12c2f0350b4954e336a97c174
Aigars Peda <aigarspeda@gmail.com>
2020-10-02 07:52:41 +0300

    Update types.ts

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,7 +1,24 @@
+import { ErrorsType, UserDataType } from "../types/types";
+
+// USER
 export const AUTHENTICATE_USER = "AUTHENTICATE_USER";
 interface IAuthenticateUserAction {
   type: typeof AUTHENTICATE_USER;
   payload: string;
 }
 
-export type AuthenticateActionTypes = IAuthenticateUserAction;
+export const SET_USER = "SET_USER";
+interface ISetUserAction {
+  type: typeof SET_USER;
+  payload: UserDataType;
+}
+
+export type AuthenticateActionTypes = IAuthenticateUserAction | ISetUserAction;
+
+// ERROR
+export const SET_ERROR = "SET_ERROR";
+interface ISetErrorAction {
+  type: typeof SET_ERROR;
+  payload: ErrorsType;
+}
+export type SetErrorActionTypes = ISetErrorAction;
