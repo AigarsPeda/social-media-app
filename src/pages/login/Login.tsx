1f1d549b3c30e53e22146b4c6851d4845a7bbfde
Aigars Peda <aigarspeda@gmail.com>
2020-10-02 07:52:14 +0300

    Update Login.tsx

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,26 +1,25 @@
 import React, { useState } from "react";
-import axios from "axios";
-import { Link, RouteComponentProps, withRouter } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
-// types
-import { ErrorsType, LoginUserType } from "../../types/types";
+// redux
+import { connect } from "react-redux";
+import { RootStateType } from "../../redux/store";
+import { logInUser } from "../../redux/actions/userAction";
 
 // components
 import Logo from "../../images/Logo";
 import Spinner from "../../components/spinner/Spinner";
 import Input from "../../components/reusable/Input";
 
-import { BASE_URL } from "../../constant";
-
-interface Props extends RouteComponentProps {}
+type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps;
 
 const Login: React.FC<Props> = (props) => {
-  const { history } = props;
+  const { logInUser, isAuthenticated, errors } = props;
   const [userData, setUserData] = useState({
     email: "",
     password: ""
   });
-  const [errors, setErrors] = useState<ErrorsType>({});
+
   const [isLoading, setIsLoading] = useState(false);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,34 +30,19 @@ const Login: React.FC<Props> = (props) => {
     }));
   };
 
-  const config = {
-    headers: {
-      "Content-Type": "application/json"
-    }
-  };
-
-  const logInUser = (user: LoginUserType) => {
-    const response = axios
-      .post(`${BASE_URL}/login`, user, config)
-      .then((res) => {
-        return res.data;
-      })
-      .catch((err) => {
-        return err.response.data;
-      });
-    return response;
-  };
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
-    // something.token or something.password or something.general
-    const something = await logInUser(userData);
-    console.log("something: ", something);
+    // User action
+    logInUser(userData);
   };
+
+  if (isAuthenticated) {
+    return <Redirect to="/" />;
+  }
 
   return (
     <div className="login">
+      {console.log("LOGIN COMPONENT: ", isAuthenticated)}
       <div className="logo">
         <Logo />
       </div>
@@ -103,4 +87,11 @@ const Login: React.FC<Props> = (props) => {
   );
 };
 
-export default withRouter(Login);
+const mapStateToProps = (state: RootStateType) => ({
+  isAuthenticated: state.user.isAuthenticated,
+  errors: state.errors.error
+});
+
+const mapDispatchToProps = { logInUser };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
