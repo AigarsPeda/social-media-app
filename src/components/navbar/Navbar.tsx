e31ad85d1c3248f037532c884beabc2e32205d8d
Aigars Peda <aigarspeda@gmail.com>
2020-10-04 15:55:06 +0300

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,25 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+// redux
+import { connect } from "react-redux";
+import { logOutUser } from "../../redux/actions/userAction";
+import { RootStateType } from "../../redux/store";
+
+// components
 import Logo from "../../images/Logo";
 
-const Navbar: React.FC = () => {
+type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps;
+
+const Navbar: React.FC<Props> = (props) => {
+  const { isAuthenticated, logOutUser } = props;
   return (
     <nav className="navbar">
       <div className="container">
         <Link to="/" className="logo">
           <Logo />
         </Link>
-        <ul className="links">
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign Up</Link>
-          </li>
-        </ul>
+        {isAuthenticated ? (
+          <button onClick={logOutUser}>Log Out</button>
+        ) : (
+          <ul className="links">
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+          </ul>
+        )}
       </div>
     </nav>
   );
 };
 
-export default Navbar;
+const mapStateToProps = (state: RootStateType) => ({
+  isAuthenticated: state.user.isAuthenticated
+});
+
+const mapDispatchToProps = { logOutUser };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
