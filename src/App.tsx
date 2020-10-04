2beed20826b7bc166f26a6e6b30a2537c798ad07
Aigars Peda <aigarspeda@gmail.com>
2020-10-04 15:54:34 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./global.styles/App.scss";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
-import jwtDecode from "jwt-decode";
 
-// types
-import { TokenType } from "./types/types";
-
-// utils
-import AuthRoute from "./utils/authRoute/AuthRoute";
+// router
+import { BrowserRouter } from "react-router-dom";
 
 // redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
 // components
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import SignUp from "./pages/signup/SignUp";
 import Navbar from "./components/navbar/Navbar";
+import AppRoutes from "./routes/AppRoutes";
 
 const App: React.FC = () => {
-  // const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  // useEffect(() => {
-  //   const token = localStorage.getItem("FBIdToken");
-  //   if (token) {
-  //     const decodedToken: TokenType = jwtDecode(token);
-  //     if (decodedToken.exp * 1000 < Date.now()) {
-  //       window.location.href = "/login";
-  //       setIsAuthenticated(false);
-  //     } else {
-  //       setIsAuthenticated(true);
-  //     }
-  //   }
-  // }, [isAuthenticated]);
-
   return (
     <BrowserRouter>
       <Provider store={store}>
         <Navbar />
         <div className="App">
-          <Switch>
-            <Route
-              exact
-              path="/"
-              component={Home}
-              // isAuthenticated={isAuthenticated}
-            />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={SignUp} />
-          </Switch>
+          <AppRoutes />
         </div>
       </Provider>
     </BrowserRouter>
