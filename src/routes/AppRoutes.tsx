cd9f9226629d8a2b3fa23e1bcc72d220b9e87229
Aigars Peda <aigarspeda@gmail.com>
2020-10-08 11:50:56 +0300

    checking if token is valid and if it is setting in to axis header

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,10 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+
+// token check
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+import { TokenType } from "../types/types";
 
 // auth route
 import AuthRoute from "../utils/authRoute/AuthRoute";
@@ -7,16 +12,34 @@ import AuthRoute from "../utils/authRoute/AuthRoute";
 // redux
 import { connect } from "react-redux";
 import { RootStateType } from "../redux/store";
+import { logOutUser } from "../redux/actions/userAction";
 
 // pages
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import SignUp from "../pages/signup/SignUp";
 
-type Props = ReturnType<typeof mapStateToProps>;
+type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps;
 
 const AppRoutes: React.FC<Props> = (props) => {
-  const { isAuthenticated } = props;
+  const { isAuthenticated, token, logOutUser } = props;
+
+  // TODO: i should check if token has not been ended
+  useEffect(() => {
+    // checking if token is valid and if it is setting in to axis header
+    if (token) {
+      console.log("ES TE");
+      const decodedToken: TokenType = jwtDecode(token);
+      if (decodedToken.exp * 1000 < Date.now()) {
+        // user action if token has expired
+        logOutUser();
+        <Redirect to="/login" />;
+      } else {
+        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      }
+    }
+  });
+
   return (
     <Switch>
       <AuthRoute
@@ -32,9 +55,10 @@ const AppRoutes: React.FC<Props> = (props) => {
 };
 
 const mapStateToProps = (state: RootStateType) => ({
-  isAuthenticated: state.user.isAuthenticated
+  isAuthenticated: state.user.isAuthenticated,
+  token: state.user.token
 });
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = { logOutUser };
 
 export default connect(mapStateToProps, mapDispatchToProps)(AppRoutes);
