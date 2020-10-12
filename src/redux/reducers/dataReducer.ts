59eaec187e120c433fffee01efc8234e4af969a5
Aigars Peda <aigarspeda@gmail.com>
2020-10-12 11:52:49 +0300

    Update dataReducer.ts

--- src/redux/reducers/dataReducer.ts
+++ src/redux/reducers/dataReducer.ts
@@ -5,7 +5,8 @@ import {
   CLEAR_DATA,
   IS_LOADING_DATA,
   LIKE_SCREAM,
-  UNLIKE_LIKE_SCREAM
+  UNLIKE_LIKE_SCREAM,
+  DELETE_SCREAM
 } from "../types";
 
 export interface IUserInitialState {
@@ -46,16 +47,23 @@ export default (state = initialState, action: SetDataTypes) => {
     // in both cases do this
     case LIKE_SCREAM:
     case UNLIKE_LIKE_SCREAM:
-      const newScreamsArray = state.screams.map((scream) => {
-        if (scream.screamId === action.payload.screamId) {
-          return (scream = action.payload);
-        } else {
-          return scream;
-        }
-      });
       return {
         ...state,
-        screams: newScreamsArray
+        screams: state.screams.map((scream) => {
+          if (scream.screamId === action.payload.screamId) {
+            return (scream = action.payload);
+          } else {
+            return scream;
+          }
+        })
+      };
+
+    case DELETE_SCREAM:
+      return {
+        ...state,
+        screams: state.screams.filter((scream) => {
+          return scream.screamId !== action.payload;
+        })
       };
 
     case CLEAR_DATA: {
