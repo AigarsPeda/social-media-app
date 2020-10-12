7e8977293568e7df4c7d748a385d964eb7615e26
Aigars Peda <aigarspeda@gmail.com>
2020-10-12 08:58:17 +0300

    added new functionality to scream component

--- src/components/scream/Scream.tsx
+++ src/components/scream/Scream.tsx
@@ -7,12 +7,24 @@ import { ScreamType } from "../../types/types";
 // helper functions
 import { dateFormatted } from "../../helpers/dateFormatted";
 
-interface Props {
+// redux
+import { connect } from "react-redux";
+import { RootStateType } from "../../redux/store";
+import { likeScream, unLikeScream } from "../../redux/actions/dataActions";
+import EmptyLikeIcon from "../../images/EmptyLikeIcon";
+import LikeIcon from "../../images/LikeIcon";
+import CommentIcon from "../../images/CommentIcon";
+import TrashCanIcon from "../../images/TrashCanIcon";
+
+type MyProps = {
   scream: ScreamType;
-}
+};
+type Props = ReturnType<typeof mapStateToProps> &
+  typeof mapDispatchToProps &
+  MyProps;
 
 const Scream: React.FC<Props> = (props) => {
-  const { scream } = props;
+  const { scream, user, likeScream, unLikeScream } = props;
   const {
     body,
     createdAt,
@@ -23,13 +35,50 @@ const Scream: React.FC<Props> = (props) => {
     commentCount
   } = scream;
 
-  // const dateFormatted = (isoDate: string) => {
-  //   dayjs.extend(relativeTime);
-  //   return dayjs(isoDate).fromNow();
-  // };
+  // TODO: disable button if user already liked scream
+  // and show different icon
+
+  const isLikedScream = (id: string) => {
+    const { likes } = user.userData;
+    if (likes.length && likes.find((like) => like.screamId === id)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const likeAndUnlikeButton = () => {
+    return !isLikedScream(screamId) ? (
+      <button onClick={() => likeScream(screamId)}>
+        <EmptyLikeIcon />
+      </button>
+    ) : (
+      <button onClick={() => unLikeScream(screamId)}>
+        <LikeIcon className="like" />
+      </button>
+    );
+  };
+
+  const isOwner = () => {
+    if (user.userData.credentials.handle === userHandle) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const deleteButton = () => {
+    return isOwner() ? (
+      <button className="btn-delete">
+        <TrashCanIcon />
+      </button>
+    ) : null;
+  };
 
   return (
     <div className="scream">
+      {console.log("Scream:", scream)}
+      {console.log("User: ", user.userData.likes)}
       <div className="img-container">
         <img src={userImage} />
       </div>
@@ -37,11 +86,32 @@ const Scream: React.FC<Props> = (props) => {
         <Link to={`/users/${userHandle}`} className="user-handle">
           {userHandle}
         </Link>
+
         <p className="created-at">{dateFormatted(createdAt)}</p>
         <p className="body">{body}</p>
+        <div className="action-container">
+          <p>
+            {likeAndUnlikeButton()}
+            {likeCount} Likes
+          </p>
+          <p>
+            <button>
+              <CommentIcon className="comments" />
+            </button>
+            {commentCount} Comments
+          </p>
+        </div>
       </div>
+      <div className="delete-container">{deleteButton()}</div>
     </div>
   );
 };
 
-export default Scream;
+const mapStateToProps = (state: RootStateType) => ({
+  user: state.user,
+  isLoadingUser: state.user.isLoadingUser
+});
+
+const mapDispatchToProps = { likeScream, unLikeScream };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Scream);
