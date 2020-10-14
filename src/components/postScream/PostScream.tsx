22e50d11432e678b12dc97ddae3b3ca6411e7644
Aigars Peda <aigarspeda@gmail.com>
2020-10-14 18:08:50 +0300

    transiton on cancel postin post

--- src/components/postScream/PostScream.tsx
+++ src/components/postScream/PostScream.tsx
@@ -3,18 +3,20 @@ import React, { useState } from "react";
 // redux
 import { connect } from "react-redux";
 import { RootStateType } from "../../redux/store";
+import { postScream } from "../../redux/actions/dataActions";
+import { clearErrors } from "../../redux/actions/errorActions";
 
 // icons
 import PlusIcon from "../../images/PlusIcon";
 import Spinner from "../spinner/Spinner";
-import { postScream } from "../../redux/actions/dataActions";
-import { clearErrors } from "../../redux/actions/errorActions";
+import CancelIcon from "../../images/CancelIcon";
 
 type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps;
 
 const PostScream: React.FC<Props> = (props) => {
   const { isLoading, errors, postScream, clearErrors } = props;
   const [isOpen, setIsOpen] = useState(false);
+  const [toggle, setToggle] = useState(true);
   const [screamBody, setScreamBody] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -25,43 +27,75 @@ const PostScream: React.FC<Props> = (props) => {
   const handleCancel = () => {
     clearErrors();
     setScreamBody("");
-    setIsOpen(false);
+
+    setToggle(false);
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 500);
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     postScream(screamBody);
-
     setScreamBody("");
-    setIsOpen(false);
+
+    setToggle(false);
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 500);
+  };
+
+  const handleOutClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const element = e.target as HTMLDivElement;
+
+    if (element.classList.contains("add-scream-modal")) {
+      setScreamBody("");
+      setToggle(false);
+      setTimeout(() => {
+        setIsOpen(false);
+      }, 500);
+    }
+  };
+
+  const handleOpen = () => {
+    setIsOpen(true);
+    setToggle(true);
   };
 
   return (
     <div className="add-scream">
-      <button onClick={() => setIsOpen((state) => !state)}>
+      <button onClick={handleOpen}>
         <PlusIcon />
       </button>
       {isOpen && (
-        <div className="add-scream-modal">
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="new scream">Add New Scream!</label>
+        <div
+          className={toggle ? "add-scream-modal" : "add-scream-modal toggleOut"}
+          onClick={handleOutClick}
+        >
+          <form
+            onSubmit={handleSubmit}
+            // className={toggle ? "toggleIn" : "toggleOut"}
+            className="toggleIn"
+          >
+            {!isLoading && (
+              <div className="cancel">
+                <span onClick={handleCancel}>
+                  <CancelIcon />
+                </span>
+              </div>
+            )}
+            <h2>Post New Scream!</h2>
             <textarea
               rows={6}
               value={screamBody}
               name="new scream"
               onChange={handleChange}
             />
-            <div className="action-btn-container">
-              {errors.body && <p>{errors.body}</p>}
-              <button type="submit" className="add-scream">
-                {isLoading ? <Spinner /> : "Add"}
-              </button>
-              {!isLoading && (
-                <button onClick={handleCancel} className="cancel">
-                  Cancel
-                </button>
-              )}
-            </div>
+
+            {errors.body && <p>{errors.body}</p>}
+            <button type="submit" className="add-scream">
+              {isLoading ? <Spinner /> : "Add"}
+            </button>
           </form>
         </div>
       )}
