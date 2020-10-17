e5110c4a234b2a6720febbe3171ac5c33f1178b7
Aigars Peda <aigarspeda@gmail.com>
2020-10-17 09:16:02 +0300

    created Comment component

---
+++ src/components/reusable/comment/Comment.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CommentIcon from "../../../images/CommentIcon";
+
+const Comment: React.FC = () => {
+  return (
+    <div className="comment">
+      <button>
+        <CommentIcon />
+      </button>
+    </div>
+  );
+};
+
+export default Comment;
