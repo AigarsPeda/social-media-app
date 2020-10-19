2e56625f038b6d156df0f785d17f2d0296c1da71
Aigars Peda <aigarspeda@gmail.com>
2020-10-19 08:55:13 +0300

    created DisplayComments component

--- src/components/displayComments/DisplayComments.tsx
+++ src/components/displayComments/DisplayComments.tsx
@@ -1,33 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import dayjs from "dayjs";
 import { Link } from "react-router-dom";
 import { ScreamComment } from "../../types/types";
+import { RootStateType } from "../../redux/store";
+import { connect } from "react-redux";
+import { submitComment } from "../../redux/actions/dataActions";
+import Input from "../reusable/Input";
 
 type MyProps = {
   comments: ScreamComment[];
+  screamId: string;
 };
+type Props = ReturnType<typeof mapStateToProps> &
+  typeof mapDispatchToProps &
+  MyProps;
 
-const DisplayComments: React.FC<MyProps> = (props) => {
-  const { comments } = props;
+const DisplayComments: React.FC<Props> = (props) => {
+  const { comments, screamId, errors, submitComment } = props;
+  const [commentBody, setCommentBody] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    submitComment(screamId, commentBody);
+    setCommentBody("");
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCommentBody(e.target.value);
+  };
+
   return (
     <div className="display-comments">
+      <div className="add-comment">
+        <form onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            autoComplete="off"
+            value={commentBody}
+            error={errors.comment}
+            errorClass="errors"
+            name="commentBody"
+            placeholder="Add Comment!"
+            handleInputChange={handleInputChange}
+          />
+          {errors.comment && <p>{errors.comment}</p>}
+          <button type="submit">Add Comment</button>
+        </form>
+      </div>
       {comments && comments.length ? (
-        comments.map((comment) => {
+        comments.map((comment, index) => {
+          const { createdAt, userHandle, userImage, body } = comment;
           return (
-            <section key={comment.createdAt} className="comments">
-              <div className="img-container">
-                <img src={comment.userImage} />
-              </div>
-              <div className="content-container">
-                <Link to={`/users/${comment.userHandle}`}>
-                  {comment.userHandle}
-                </Link>
-                <p className="comment-body">{comment.body}</p>
-                <p className="created-at">
-                  {dayjs(comment.createdAt).format("HH:mm, DD MMMM YYYY")}
-                </p>
-              </div>
-            </section>
+            <div key={createdAt}>
+              <section className="comments">
+                <div className="img-container">
+                  <img src={userImage} />
+                </div>
+                <div className="content-container">
+                  <Link to={`/users/${userHandle}`}>{userHandle}</Link>
+                  <p className="comment-body">{body}</p>
+                  <p className="created-at">
+                    {dayjs(createdAt).format("HH:mm, DD MMMM YYYY")}
+                  </p>
+                </div>
+              </section>
+              {index !== comments.length - 1 && <hr />}
+            </div>
           );
         })
       ) : (
@@ -39,4 +77,11 @@ const DisplayComments: React.FC<MyProps> = (props) => {
   );
 };
 
-export default DisplayComments;
+const mapStateToProps = (state: RootStateType) => ({
+  user: state.user.userData.credentials,
+  errors: state.errors.error
+});
+
+const mapDispatchToProps = { submitComment };
+
+export default connect(mapStateToProps, mapDispatchToProps)(DisplayComments);
