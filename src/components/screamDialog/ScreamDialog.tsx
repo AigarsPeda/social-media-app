9c3087f0993eef2da06e3b578db7d907e820ce76
Aigars Peda <aigarspeda@gmail.com>
2020-10-19 09:55:55 +0300

    Update ScreamDialog.tsx

--- src/components/screamDialog/ScreamDialog.tsx
+++ src/components/screamDialog/ScreamDialog.tsx
@@ -1,13 +1,25 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 
 // redux
 import { connect } from "react-redux";
-import { getScream } from "../../redux/actions/dataActions";
+import {
+  getScream,
+  likeScream,
+  unLikeScream
+} from "../../redux/actions/dataActions";
 import { RootStateType } from "../../redux/store";
-import Spinner from "../spinner/Spinner";
+
+// icons
+import CancelIcon from "../../images/CancelIcon";
 import DownArrowIcon from "../../images/DownArrowIcon";
+
+// components
+import Spinner from "../spinner/Spinner";
+import Comment from "../reusable/comment/Comment";
+import LikeAndUnlikeButton from "../reusable/likeAndUnlikeButton/LikeAndUnlikeButton";
+import DisplayComments from "../displayComments/DisplayComments";
 
 type MyProps = {
   screamId: string;
@@ -19,18 +31,43 @@ type Props = ReturnType<typeof mapStateToProps> &
   MyProps;
 
 const ScreamDialog: React.FC<Props> = (props) => {
-  const { isLoading, screamId, scream, getScream } = props;
+  const {
+    isLoading,
+    screamId,
+    scream,
+    user,
+    getScream,
+    likeScream,
+    unLikeScream
+  } = props;
   const [isOpen, setIsOpen] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
   const handleOpen = () => {
     getScream(screamId);
     setIsOpen(true);
+    setToggle(true);
 
     console.log(scream);
   };
 
   const handleClose = () => {
-    setIsOpen(false);
+    setToggle(false);
+
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 500);
+  };
+
+  const handleOutClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const element = e.target as HTMLDivElement;
+
+    if (element.classList.contains("scream-dialog-modal")) {
+      setToggle(false);
+      setTimeout(() => {
+        setIsOpen(false);
+      }, 500);
+    }
   };
 
   return (
@@ -39,18 +76,54 @@ const ScreamDialog: React.FC<Props> = (props) => {
         <DownArrowIcon />
       </button>
       {isOpen && (
-        <div className="scream-dialog-modal">
-          <div className="dialog-content">
+        <div className="scream-dialog-modal" onClick={handleOutClick}>
+          <div
+            className={
+              toggle ? "dialog-content toggleIn" : "dialog-content toggleOut"
+            }
+          >
             {isLoading ? (
               <div className="spinner">
                 <Spinner />
               </div>
             ) : (
               <div>
-                <Link to={`/users/${scream.userHandle}`}>
-                  {scream.userHandle}
-                </Link>
-                <button onClick={handleClose}>Close</button>
+                <div className="cancel">
+                  <span onClick={handleClose}>
+                    <CancelIcon />
+                  </span>
+                </div>
+                <div className="main">
+                  <div className="scream-dialog-img">
+                    <img src={scream.userImage} />
+                  </div>
+                  <div className="content-container">
+                    <Link to={`/users/${scream.userHandle}`}>
+                      {scream.userHandle}
+                    </Link>
+                    <p>
+                      {dayjs(scream.createdAt).format("HH:mm, DD MMMM YYYY")}
+                    </p>
+                    <section>
+                      <p className="body">{scream.body}</p>
+                      <div className="like-comment-container">
+                        <LikeAndUnlikeButton
+                          likes={user.userData.likes}
+                          id={screamId}
+                          likeScream={likeScream}
+                          unLikeScream={unLikeScream}
+                        />
+                        <p>{scream.likeCount}</p>
+                        <Comment />
+                        <p>{scream.commentCount}</p>
+                      </div>
+                    </section>
+                  </div>
+                </div>
+                <DisplayComments
+                  comments={scream.comments}
+                  screamId={screamId}
+                />
               </div>
             )}
           </div>
@@ -62,9 +135,10 @@ const ScreamDialog: React.FC<Props> = (props) => {
 
 const mapStateToProps = (state: RootStateType) => ({
   isLoading: state.ui.isLoading,
-  scream: state.data.scream
+  scream: state.data.scream,
+  user: state.user
 });
 
-const mapDispatchToProps = { getScream };
+const mapDispatchToProps = { getScream, likeScream, unLikeScream };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ScreamDialog);
